64be11399c201e3f177b5c86da53bfa2ec2dd416
Gabriel Pescaroli <ShaickMiles@gmail.com>
2023-01-04 13:56:00 -0300

    detalhe ficha

--- src/app/db/db.ts
+++ src/app/db/db.ts
@@ -1,5 +1,17 @@
 import Dexie, { Table } from 'dexie';
 
+export enum treinamento{
+  leigo = 0,
+  treinado = 1,
+  experto = 2
+}
+
+export enum origemHabilidade{
+  classe = 0,
+  origem = 1,
+  outros = 2
+}
+
 export interface Ficha {
   id?: number;
   nome: string;
@@ -17,43 +29,54 @@ export interface Ficha {
   peAtual?: number;
   defesa?: number;
   sanidade?: number;
-  tAcrobacia?: number;
-  tAdestramento?: number;
-  tArtes?: number;
-  tAtletismo?: number;
-  tAtualidade?: number;
-  tCiencia?: number;
-  tCrime?: number;
-  tDiplomacia?: number;
-  tEnganacao?: number;
-  tFortitude?: number;
-  tFurtividade?: number;
-  tIniciativa?: number;
-  tIntimidacao?: number;
-  tIntuicao?: number;
-  tInvestigacao?: number;
-  tLuta?: number;
-  tMedicina?: number;
-  tOcultismo?: number;
-  tPercepcao?: number;
-  tPilotagem?: number;
-  tPontaria?: number;
-  tProfissao?: number;
-  tReflexo?: number;
-  tReligiao?: number;
-  tSobrevivencia?: number;
-  tTatica?: number;
-  tTecnologia?: number;
-  tVontade?: number
+  sanidadeAtual?: number;
+  tAcrobacia?: treinamento;
+  tAdestramento?: treinamento;
+  tArtes?: treinamento;
+  tAtletismo?: treinamento;
+  tAtualidade?: treinamento;
+  tCiencia?: treinamento;
+  tCrime?: treinamento;
+  tDiplomacia?: treinamento;
+  tEnganacao?: treinamento;
+  tFortitude?: treinamento;
+  tFurtividade?: treinamento;
+  tIniciativa?: treinamento;
+  tIntimidacao?: treinamento;
+  tIntuicao?: treinamento;
+  tInvestigacao?: treinamento;
+  tLuta?: treinamento;
+  tMedicina?: treinamento;
+  tOcultismo?: treinamento;
+  tPercepcao?: treinamento;
+  tPilotagem?: treinamento;
+  tPontaria?: treinamento;
+  tProfissao?: treinamento;
+  tReflexo?: treinamento;
+  tReligiao?: treinamento;
+  tSobrevivencia?: treinamento;
+  tTatica?: treinamento;
+  tTecnologia?: treinamento;
+  tVontade?: treinamento
+}
+
+export interface Habilidade{
+  id?: number;
+  idFicha: number;
+  origem: origemHabilidade;
+  titulo: string;
+  descricao: string;
 }
 
 export class AppDB extends Dexie {
   fichas!: Table<Ficha, number>;
+  habilidades!: Table<Habilidade, number>;
 
   constructor() {
     super('fichaparanormal');
     this.version(3).stores({
-      fichas: "++id"
+      fichas: "++id",
+      habilidades: "++id, idFicha"
     });
     this.on('populate', () => this.populate());
   }
--- src/app/ficha-detail/ficha-detail.component.ts
+++ src/app/ficha-detail/ficha-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { liveQuery } from 'dexie';
-import { db, Ficha } from '../db/db';
+import { db, Ficha, Habilidade, origemHabilidade, treinamento } from '../db/db';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -13,7 +13,12 @@ export class FichaDetailComponent {
     private route: ActivatedRoute
   ) { }
 
+  tituloHabilidade: string = "";
+  origemSHabilidade: origemHabilidade = origemHabilidade.classe;
+  descricaoHabilidade: string = "";
+
   ficha: Ficha = { nome: "" };
+  habilidades: Habilidade[] = [];
 
   classe: string = "";
   agilidade: number = 0;
@@ -21,34 +26,164 @@ export class FichaDetailComponent {
   presenca: number = 0;
   vigor: number = 0;
   intelecto: number = 0;
-  
+
   pv: number = 0;
   pvAtual: number = 0;
   pe: number = 0;
   peAtual: number = 0;
+  sanidade: number = 0;
+  sanidadeAtual: number = 0;
+  defesa: number = 0;
+
+  tAcrobacia: treinamento = treinamento.leigo;
+  tAdestramento: treinamento = treinamento.leigo;
+  tArtes: treinamento = treinamento.leigo;
+  tAtletismo: treinamento = treinamento.leigo;
+  tAtualidade: treinamento = treinamento.leigo;
+  tCiencia: treinamento = treinamento.leigo;
+  tCrime: treinamento = treinamento.leigo;
+  tDiplomacia: treinamento = treinamento.leigo;
+  tEnganacao: treinamento = treinamento.leigo;
+  tFortitude: treinamento = treinamento.leigo;
+  tFurtividade: treinamento = treinamento.leigo;
+  tIniciativa: treinamento = treinamento.leigo;
+  tIntimidacao: treinamento = treinamento.leigo;
+  tIntuicao: treinamento = treinamento.leigo;
+  tInvestigacao: treinamento = treinamento.leigo;
+  tLuta: treinamento = treinamento.leigo;
+  tMedicina: treinamento = treinamento.leigo;
+  tOcultismo: treinamento = treinamento.leigo;
+  tPercepcao: treinamento = treinamento.leigo;
+  tPilotagem: treinamento = treinamento.leigo;
+  tPontaria: treinamento = treinamento.leigo;
+  tProfissao: treinamento = treinamento.leigo;
+  tReflexo: treinamento = treinamento.leigo;
+  tReligiao: treinamento = treinamento.leigo;
+  tSobrevivencia: treinamento = treinamento.leigo;
+  tTatica: treinamento = treinamento.leigo;
+  tTecnologia: treinamento = treinamento.leigo;
+  tVontade: treinamento = treinamento.leigo
+
+
 
 
   fichaObserv = liveQuery(() => this.loadFicha()).subscribe((ficha) => {
-     this.ficha = (ficha || { nome: "" })
-     this.classe = ficha?.classe || "";
-     this.agilidade = ficha?.agilidade || 0;
-     this.forca = ficha?.forca || 0;
-     this.presenca = ficha?.presenca || 0;
-     this.vigor = ficha?.vigor || 0;
-     this.intelecto = ficha?.intelecto || 0;
+    this.ficha = (ficha || { nome: "" })
+    this.classe = ficha?.classe || "";
+    this.agilidade = ficha?.agilidade || 0;
+    this.forca = ficha?.forca || 0;
+    this.presenca = ficha?.presenca || 0;
+    this.vigor = ficha?.vigor || 0;
+    this.intelecto = ficha?.intelecto || 0;
 
-     this.pv = ficha?.pv || 0;
-     this.pvAtual = ficha?.pvAtual || 0;
-     this.pe = ficha?.pe || 0;
-     this.peAtual = ficha?.peAtual || 0;
-     
-     })
+    this.pv = ficha?.pv || 0;
+    this.pvAtual = ficha?.pvAtual || 0;
+    this.pe = ficha?.pe || 0;
+    this.peAtual = ficha?.peAtual || 0;
+    this.sanidade = ficha?.sanidade || 0;
+    this.sanidadeAtual = ficha?.sanidadeAtual || 0;
+    this.defesa = ficha?.defesa || 0;
+
+    this.tAcrobacia = ficha?.tAcrobacia || 0;
+    this.tAdestramento = ficha?.tAdestramento || 0;
+    this.tArtes = ficha?.tArtes || 0;
+    this.tAtletismo = ficha?.tAtletismo || 0;
+    this.tAtualidade = ficha?.tAtualidade || 0;
+    this.tCiencia = ficha?.tCiencia || 0;
+    this.tCrime = ficha?.tCrime || 0;
+    this.tDiplomacia = ficha?.tDiplomacia || 0;
+    this.tEnganacao = ficha?.tEnganacao || 0;
+    this.tFortitude = ficha?.tFortitude || 0;
+    this.tFurtividade = ficha?.tFurtividade || 0;
+    this.tIniciativa = ficha?.tIniciativa || 0;
+    this.tIntimidacao = ficha?.tIntimidacao || 0;
+    this.tInvestigacao = ficha?.tInvestigacao || 0;
+    this.tLuta = ficha?.tLuta || 0;
+    this.tMedicina = ficha?.tMedicina || 0;
+    this.tOcultismo = ficha?.tOcultismo || 0;
+    this.tPercepcao = ficha?.tPercepcao || 0;
+    this.tPilotagem = ficha?.tPilotagem || 0;
+    this.tPontaria = ficha?.tPontaria || 0;
+    this.tProfissao = ficha?.tProfissao || 0;
+    this.tReflexo = ficha?.tProfissao || 0;
+    this.tReligiao = ficha?.tReligiao || 0;
+    this.tSobrevivencia = ficha?.tSobrevivencia || 0;
+    this.tTatica = ficha?.tTatica || 0;
+    this.tTecnologia = ficha?.tTecnologia || 0;
+    this.tVontade = ficha?.tVontade || 0;
+
+  });
+
+  habilidadesObserv = liveQuery(()=>this.loadHabilidades())
 
   async loadFicha() {
     return db.fichas.get(Number.parseInt((this.route.snapshot.paramMap.get("id") || "").toString()));
   }
 
+  async loadHabilidades(){
+    return db.habilidades.where({idFicha: Number.parseInt((this.route.snapshot.paramMap.get("id") || "").toString())}).toArray();
+  }
+
   async saveFicha() {
-    db.fichas.update(Number.parseInt((this.route.snapshot.paramMap.get("id") || "").toString()), { classe: this.classe, agilidade: this.agilidade, forca: this.forca, presenca: this.presenca, vigor: this.vigor, intelecto: this.intelecto, pv: this.pv, pvAtual: this.pvAtual, pe: this.pe, peAtual: this.peAtual });
+    db.fichas.update(Number.parseInt((this.route.snapshot.paramMap.get("id") || "").toString()),
+      {
+        classe: this.classe,
+        agilidade: this.agilidade,
+        forca: this.forca,
+        presenca: this.presenca,
+        vigor: this.vigor,
+        intelecto: this.intelecto,
+        pv: this.pv,
+        pvAtual: this.pvAtual,
+        pe: this.pe,
+        peAtual: this.peAtual,
+        sanidade: this.sanidade,
+        sanidadeAtual: this.sanidadeAtual,
+        defesa: this.defesa,
+        tAcrobacia: this.tAcrobacia,
+        tAdestramento: this.tAdestramento,
+        tArtes: this.tArtes,
+        tAtletismo: this.tAtletismo,
+        tAtualidade: this.tAtualidade,
+        tCiencia: this.tCiencia,
+        tCrime: this.tCrime,
+        tDiplomacia: this.tDiplomacia,
+        tEnganacao: this.tEnganacao,
+        tFortitude: this.tFortitude,
+        tFurtividade: this.tFurtividade,
+        tIniciativa: this.tIniciativa,
+        tIntimidacao: this.tIntimidacao,
+        tIntuicao: this.tIntuicao,
+        tInvestigacao: this.tInvestigacao,
+        tLuta: this.tLuta,
+        tMedicina: this.tMedicina,
+        tOcultismo: this.tOcultismo,
+        tPercepcao: this.tPercepcao,
+        tPilotagem: this.tPilotagem,
+        tPontaria: this.tPontaria,
+        tProfissao: this.tProfissao,
+        tReflexo: this.tReflexo,
+        tReligiao: this.tReligiao,
+        tSobrevivencia: this.tSobrevivencia,
+        tTatica: this.tTatica,
+        tTecnologia: this.tTecnologia,
+        tVontade: this.tVontade
+      });
+  }
+
+  async saveHabilidade(){
+    await db.habilidades.add({
+      idFicha: Number.parseInt((this.route.snapshot.paramMap.get("id") || "").toString()),
+      origem: this.origemSHabilidade,
+      titulo: this.tituloHabilidade,
+      descricao: this.descricaoHabilidade
+    });
+    this.tituloHabilidade = "";
+    this.origemSHabilidade = origemHabilidade.classe;
+    this.descricaoHabilidade = "";
+  }
+
+  async deleteHabilidade(id: number | undefined){
+    await db.habilidades.delete(Number.parseInt((id || "").toString()));
   }
 }
